0f64fc2e2e67b107e0e5af17ac9dafc25033da86
WebersIO <muzammilmetar82@gmail.com>
2025-07-09 17:27:04 +0530

    completed

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express")
+const path = require("path")
+const cookieParser = require("cookie-parser")
+const port = 10000;
+
+const app = express();
+
+app.set("view engine", "ejs")
+
+app.use(express.urlencoded({ extended: true }))
+
+// app.use(cookieParser())
+
+app.use("/", express.static(path.join(__dirname, "/public")))
+
+// app.use("/", require("./routes/admin"))
+
+
+app.listen(port, (err) => {
+    if(err){
+        console.log(err);
+        return false
+    }
+    console.log("server is connected on port: " + port )
+})
---
+++ routes/admin.js
@@ -0,0 +1,9 @@
+const express = require("express");
+
+const routes = express.Router();
+// const multer = require("multer")
+// const path = require("path");
+// const adminTbl = require("../models/adminTbl")
+
+routes.use(express.urlencoded())
+
